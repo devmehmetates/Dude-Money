ff9e77aea61ab6ce3fb5fc086532efba201e71df
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-08-15 21:38:49 +0300

    add local save service

---
+++ Dude-Money/Models/LocalSaveService.swift
@@ -0,0 +1,41 @@
+//
+//  LocalSaveService.swift
+//  Dude-Money
+//
+//  Created by Mehmet Ateş on 15.08.2022.
+//
+
+import Foundation
+
+class LocalSaveService {
+    
+    static func saveUser(_ people: People) {
+        let userDataKey = "userData"
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let fileURL = dir.appendingPathComponent(userDataKey)
+            
+            do {
+                if let encoded = try? JSONEncoder().encode(people) { try encoded.write(to: fileURL) }
+            } catch {
+                print("save error")
+            }
+        }
+    }
+    
+    static func readUser() -> People? {
+        let userDataKey = "userData"
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let fileURL = dir.appendingPathComponent(userDataKey)
+            do{
+                let stringContent = try String(contentsOf: fileURL, encoding: .utf8)
+                if let decoded = try? JSONDecoder().decode(People.self, from: stringContent.data(using: .utf8) ?? Data()) {
+                    return decoded
+                }
+            } catch{
+                print("read error")
+                return People.exampleModel
+            }
+        }
+        return People.exampleModel
+    }
+}
